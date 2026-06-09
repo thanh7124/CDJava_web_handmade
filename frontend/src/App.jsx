27eab37cee02586e6995873e0fd22b2c29012a72
Nguyen Phi Long <22130174@st.hcmuaf.edu.vn>
2026-06-09 20:10:00 +0700

    Cập nhật route checkout và cấu hình provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { AuthProvider } from "./context/AuthContext";
-import { CartProvider } from "./context/CartContext";
-import { FavoriteProvider } from "./context/FavoriteContext";
-
 import Home from "./views/user/Home";
 import ListProduct from "./views/user/ListProduct";
 import ProductDetail from "./views/user/ProductDetail";
@@ -13,7 +9,8 @@ import Register from "./views/user/Register";
 import Checkout from "./views/user/Checkout";
 import About from "./views/user/About";
 import Profile from "./views/user/Profile";
-import Dashboard from "./views/admin/dashboard";
+
+import Dashboard from "./views/admin/Dashboard";
 import ManageProducts from "./views/admin/ManageProducts";
 import ManageOrders from "./views/admin/ManageOrders";
 import ManageUsers from "./views/admin/ManageUsers";
@@ -22,28 +19,27 @@ import ManageCategories from "./views/admin/ManageCategories";
 function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <FavoriteProvider>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/products" element={<ListProduct />} />
-              <Route path="/products/:id" element={<ProductDetail />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/checkout" element={<Checkout />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/manage-products" element={<ManageProducts />} />
-              <Route path="/manage-orders" element={<ManageOrders />} />
-              <Route path="/manage-users" element={<ManageUsers />} />
-              <Route path="/manage-categories" element={<ManageCategories />} />
-            </Routes>
-          </FavoriteProvider>
-        </CartProvider>
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/products" element={<ListProduct />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/manage-products" element={<ManageProducts />} />
+        <Route path="/manage-orders" element={<ManageOrders />} />
+        <Route path="/manage-users" element={<ManageUsers />} />
+        <Route path="/manage-categories" element={<ManageCategories />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
+import { FavoriteProvider } from "./context/FavoriteContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <CartProvider>
-        <App />
+        <FavoriteProvider>
+          <App />
+        </FavoriteProvider>
       </CartProvider>
     </AuthProvider>
   </React.StrictMode>
